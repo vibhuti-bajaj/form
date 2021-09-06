f209b2bfb47a84c5b2bc45cdf92c61eef41fd974
vibhuti-bajaj <79647233+vibhuti-bajaj@users.noreply.github.com>
2021-09-06 18:15:14 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form2 from './Form2'
+
+const Form = () => {
+    return (
+        <div style={{border:"2px solid",width:"467",height:"612",borderColor:"red",backgroundColor:"greenyellow" ,margin:"50px",marginLeft:"400px",marginRight:"400px",flex:"center",alignContent:"center",alignSelf:"center"}} className="card">
+          <Form2/>  
+        </div>
+    )
+}
+
+export default Form
